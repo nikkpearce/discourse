ee60e9f15e8e95224d168092df8920ec2216baa9
Nikki <nikkpearce@gmail.com>
2016-02-03 11:00:27 -0800

    Discourse

---
+++ shopping-cart/products.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('init.php');
+loadScripts();
+
+    $data = array("status" => "not set!");
+
+    if(Utils::isGET()) {
+        $pm = new ProductManager();
+        $rows = $pm->listProducts();
+
+        $html = "";
+        foreach($rows as $row) {
+            $sku = $row['SKU'];
+            $price = $row['item_price'];
+            $desc = $row['description'];
+            $image = $row['image'];
+            $html .= "
+                        <div class='col s2'>
+                        <div data-sku-desc='$sku'>$desc</div>
+                        <div data-sku-price='$sku'>$price</div>
+                        <div><data-sku-image=$sku'><img src='$image'/></div>
+                        <div><input data-sku-add='$sku' type='button' value='Add'/></div>
+                      </div>";
+        }
+        echo $html;
+        return;
+
+    } else {
+        $data = array("status" => "error", "msg" => "Only GET allowed.");
+
+    }
+
+    echo json_encode($data, JSON_FORCE_OBJECT);
+
+?>
